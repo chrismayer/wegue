6fe67ba8a24c3abc22a4c9c8f815a5c3171be1fe
Christian Mayer <chris@meggsimum.de>
2020-03-05 15:40:40 +0100

    WIP: load app-conf from app folder

--- build/dev-server.js
+++ build/dev-server.js
@@ -65,6 +65,12 @@ app.use(hotMiddleware)
 var staticPath = path.posix.join(config.dev.assetsPublicPath, config.dev.assetsSubDirectory)
 app.use(staticPath, express.static('./static'))
 
+console.log(config.dev.assetsPublicPath);
+// assetsSubDirectory: 'static',
+// assetsPublicPath: './',
+var staticPath2 = path.posix.join(config.dev.assetsPublicPath, 'app')
+app.use(staticPath2, express.static('./app'))
+
 var uri = 'http://localhost:' + port
 
 var _resolve
