7895997c286bd6039bf73927e2a710ad44223ddc
Christian Mayer <chris@meggsimum.de>
2022-12-16 17:31:21 +0100

    WIP: Adapt README script for init:app

--- util/init-app.js
+++ util/init-app.js
@@ -1,3 +1,4 @@
+const { execSync } = require('child_process');
 const copydir = require('copy-dir');
 
 // keep add time and modify time
@@ -9,5 +10,45 @@ copydir('app-starter', 'app', {
   cover: false
 }, function (err) {
   if (err) throw err;
+  adaptReadme();
   console.log('done');
 });
+
+/**
+ *
+ */
+function adaptReadme () {
+  const fs = require('fs');
+  const execSync = require('child_process').execSync;
+  const readmeFile = 'app/README.md';
+  fs.truncate(readmeFile, 0, function () {
+    const template =
+`
+# Wegue App
+
+This directory contains a Wegue based app.
+
+* App created at: __CREATED_AT__
+* Current Wegue commit: __WEGUE_COMMIT__
+* Initial Wegue commit: __WEGUE_COMMIT__
+* Prefix for app-specific commits: <place-your-commit-prefix-here-or-remove-line>
+`;
+    const now = new Date();
+    let text = template.replace('__CREATED_AT__', now.toISOString());
+
+    try {
+      const gitCommand = 'git rev-parse HEAD';
+      let commit = execSync(gitCommand).toString();
+      commit = commit.trim()
+      text = text.replaceAll('__WEGUE_COMMIT__', commit);
+    } catch (error) {
+      console.warn(`Could not retrieve git commit automatically -
+        ensure you lace it yourself in the ${readmeFile}.`);
+    }
+
+    fs.writeFile(readmeFile, text, function (err) {
+      if (err) return console.log(err);
+      console.log('Hello World > helloworld.txt');
+    });
+  });
+}
